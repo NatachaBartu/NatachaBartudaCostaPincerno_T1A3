6af9a75850be794f63a99f6c707b16e9fd32f08a
natacha <nat_bartu@hotmail.com>
2021-05-18 15:24:49 +1000

    add spec file and gem file

---
+++ spec/test/spec.rb
@@ -0,0 +1,9 @@
+require_relative 'user_info.index.rb'
+require_relative 'calculate_calories.rb'
+require_relative 'quit_program.rb'
+
+Describe user_info do
+    it 'should get the user information and store' do
+    expect  
+    end
+end
---
+++ user_info.index.rb
@@ -0,0 +1,6 @@
+puts "Hello Welcome to Get track on Diet!"
+puts 
+puts "Please, write your gender and age using commas"
+user_gender = gets.chomp 
+user_age = gets.chomp
+puts "Hello your status is #{user_gender}, #{user_age}" 
