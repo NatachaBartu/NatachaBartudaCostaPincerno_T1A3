2b6e9024442b210effe0d77b4f7dff38907d7296
natacha <nat_bartu@hotmail.com>
2021-05-20 12:10:37 +1000

    changes on method definition user_info file

--- user_info.rb
+++ user_info.rb
@@ -2,39 +2,62 @@
 # require_relative '../calculate_calories.rb'
 # require_relative '../quit_program.rb'
 
-#welcome to app
-#get the users information and store in instance to reuse in others files on this code
-#ask the user wich goals he want to approach?
-#create if statement for options - get fit or lose weight
+#***********************************************************************
+# **user_info**
+
+# 1. welcome message
+# 2. ask name from users and store the information
+# 3. ask user "what is your goal" and give a 2 suggestion (get fit or lose weight)
+# 4. define this information as a number and he/she can choose as a number ( 1. get fit or 2 . lose weight )
+# 5. use the information to redirect user.
+# 6. classified the information as global variable instances ( use info later)
+# 7. use the information on case statement or if statement.
+
 class User_info
-    def greeting(user_info)
-        puts "Hello Welcome to Get track on Diet!"
-        puts
-        puts "Please, write your gender"
-        user_gender = gets.chomp
-        puts "Please, write your age"
-        user_age = gets.chomp.to_i
-        puts "Hello your status is #{user_gender}, #{user_age}" 
-    end    
- 
-    def user_info (user_gender, user_age)
-        @user_gender = user_gender
-        @user_age = user_age
+      def initialize(name, age)
+        @name = name
+        @age = age
+      end
+
+      def menu(user_info)
+        menu = ["Get fit", "Lose Weight"]
+            puts "Choose from menu, please write a number"
+            menu.each_with_index do |item, index|
+            puts "#{index + 1} - #{item}"
+        end
+end      
+    
+
+    # def user_info_menu
+    #     @user_info_menu = user_info_menu 
+    # end
+   
+    while  user_info == []
+        menu_item = gets.chomp.to_i 
+        puts "You choose  #{menu[menu_item-1]}"
+        order.push(menu[menu_item-1])
+
     end
- end
 
  user_info = User_info.new()
  user_info.greeting(user_info)
-
-case users
-
-when 18..30
-    puts " "
-
-when 31..40
-    puts " "
-
-when 41..60
-    puts " "
  
-end
+ puts "Welcome to Get track on Diet!"
+        puts
+        puts "What is your name? "
+        user_name = gets.chomp
+        puts "What is you goal?"
+        puts
+
+# case users
+
+# when 18..30
+#     puts " "
+
+# when 31..40
+#     puts " "
+
+# when 41..60
+#     puts " "
+ 
+# end
