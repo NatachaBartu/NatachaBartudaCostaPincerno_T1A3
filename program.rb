b5d663d23a80feb95094220cde88353fb8fa4c17
natacha <nat_bartu@hotmail.com>
2021-05-22 15:16:55 +1000

    add colorize gems

--- program.rb
+++ program.rb
@@ -1,7 +1,7 @@
 require_relative 'user.rb'
 require_relative 'menu.rb'
 require_relative 'calories_calculator.rb'
-
+require 'colorize'
 
 # require_relative '../calculate_calories.rb'
 # require_relative '../quit_program.rb'
@@ -24,74 +24,74 @@ class Program
     end
 
     def welcome
-        puts "Welcome to Get track on Diet!"
+        puts "Welcome to Get track on Diet!".colorize(:purple)
         puts
     end
 
     def loadUserData
-        puts "What is your name? "
+        puts "What is your name? ".colorize(:light_blue)
         user_name = gets.chomp
         @user = User.new(user_name)
-        puts "What you gender? (Male, Female)"
+        puts "What you gender? (Male, Female)".colorize(:light_blue)
         user_gender = gets.chomp
         while (@user.setGender(user_gender) == "")
-            puts "Ops! Wrong answer try again. (Male, Female)"
+            puts "Ops! Wrong answer try again. (Male, Female)".colorize(:red)
             user_gender = gets.chomp
         end
     end
 
     def menu
         
-        puts "What is you goal?"
+        puts "What is you goal?".colorize(:light_blue)
         puts
 
-        puts "Choose from menu, please write a number"
+        puts "Choose from menu, please write a number".colorize(:light_blue)
         @menu.items.each_with_index do |item, index|
             puts "#{index + 1} - #{item}"
         end
         user_info = []
         menu_item_input = gets.chomp
         while (@menu.select(menu_item_input) == "")
-            puts "Ops! Wrong answer try again."
+            puts "Ops! Wrong answer try again.".colorize(:red)
             menu_item_input = gets.chomp
         end
-        puts "You choose #{@menu.selected}"
+        puts "You choose #{@menu.selected}".colorize(:light_blue)
         puts
 
         @menu.selected
     end
 
     def store_goal
-        puts "Let's create your body profile!"
+        puts "Let's create your body profile!".colorize(:light_blue)
         puts
-        puts "Please write your goal weight"
+        puts "Please write your goal weight".colorize(:light_blue)
         @weight = gets.chomp.to_i
         puts 
-        puts "Thank you for the information!" 
+        puts "Thank you for the information!".colorize(:light_blue) 
         puts
     end
 
     def calculation
         calculator = CaloriesCalculator.new(@user)
         calories = calculator.calculateCalories(@weight)
-        puts "To lose weight, your daily calories needs are #{calories}!"
+        puts "To lose weight, your daily calories needs are #{calories}!".colorize(:red)
         puts
     end
 
     def choose_Menu
-        puts "Would like some menu ideas?"
+        puts "Would like some menu ideas?".colorize(:light_blue) 
         @menu.options.each_with_index do |item, index|
             puts "#{index + 1} - #{item}"
         end
         answer_menu_input = gets.chomp
         while (answer_menu_input != "1" && answer_menu_input != "2")
-            puts "Ops! Wrong answer try again."
+            puts "Ops! Wrong answer try again.".colorize(:light_blue) 
             answer_menu_input = gets.chomp
         end
 
         if answer_menu_input == "1"
             puts ""
-            puts "Please, choose one menu:"
+            puts "Please, choose one menu:".colorize(:light_blue) 
             @menu.get_menu.each_with_index do |item, index|
                 puts "#{index + 1} - #{item}"
             end
@@ -99,12 +99,12 @@ class Program
 
             get_menu_input = gets.chomp
             while (get_menu_input != "1" && get_menu_input != "2")
-                puts "Ops! Wrong answer try again."
+                puts "Ops! Wrong answer try again.".colorize(:red)
                 get_menu_input = gets.chomp
             end
 
             if (get_menu_input == "1")
-                @menu.nonveganMenu
+                @meonveganMenu
             else
                 @menu.veganMenu
             end
@@ -113,14 +113,14 @@ class Program
     end
 
     def get_fit
-        puts "Would like some tips suggestion?"
+        puts "Would like some tips suggestion?".colorize(:light_blue) 
         puts
         @menu.get_fit.each_with_index do |item, index|
             puts "#{index + 1} - #{item}"
         end
         answer_get_fit_input = gets.chomp
         while (answer_get_fit_input != "1" && answer_get_fit_input != "2")
-            puts "Ops! Wrong answer try again."
+            puts "Ops! Wrong answer try again.".colorize(:red) 
             answer_get_fit_input = gets.chomp
         end
 
@@ -131,7 +131,7 @@ class Program
     end
 
     def thanks
-        puts "Thanks for using the app"
+        puts "Thanks for using the app".colorize(:light_blue) 
     end
 end      
     
